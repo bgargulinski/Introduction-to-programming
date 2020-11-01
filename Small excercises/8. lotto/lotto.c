171a139eb5889cb076bea22de8741181475052aa
Bgargulinski <bartosz.gargulinski@gmail.com>
2020-11-01 10:37:19 +0100

    ex 8 formatting

--- Small excercises/8. lotto/lotto.c
+++ Small excercises/8. lotto/lotto.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 
-//function simulates loterry with random n lottery tickets
+//function simulates classic Polish loterry with random n lottery tickets
 int main() {
-    int n, i, j, k, c;
-    int result[6], ticket[6], zlicz[7];
+    int n, i, j, k, countIndex;
+    int result[6], ticket[6], count[7];
     srand((unsigned) time(NULL));
 
     scanf("%d", &n);
 
     for (i = 0; i < 7; i++)
-        zlicz[i] = 0;
+        count[i] = 0;
 
     for (i = 0; i < 6; i++) {
         j = 0;
@@ -27,25 +27,29 @@ int main() {
         printf("%d ", result[i]);
 
     for (k = 0; k < n; k++) {
-        c = 0;
+        countIndex = 0;
         for (i = 0; i < 6; i++) {
             j = 0;
             ticket[i] = (rand() % 49 + 1);
+            
             while (j < i) {
                 if (ticket[i] == ticket[j])
                     ticket[i] = (rand() % 49 + 1);
                 else j++;
             }
+
             j = 0;
             while (j < 6) {
-                if (ticket[i] == result[j])c++;
+                if (ticket[i] == result[j])
+                    countIndex++;
                 j++;
             }
         }
-        zlicz[c]++;
+        count[countIndex]++;
     }
 
     for (i = 0; i < 7; i++)
-        printf("\n%d  :%d", i, zlicz[i]);
+        printf("\n%d  :%d", i, count[i]);
+
     return 0;
 }
